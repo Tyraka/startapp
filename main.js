3e7ff02b2ff7a99540ad086c220b31c896f3ea97
Jacob <kuba300698@gmail.com>
2018-09-05 15:26:48 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+  
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
+  
+    // and load the index.html of the app.
+    win.loadFile('./src/index.html')
+
+    win.on('closed', () => {
+      // Dereference the window objec      
+      win = null
+    })
+  }
+  
+  app.on('ready', createWindow)
+ // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
