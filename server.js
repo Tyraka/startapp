e3d3bcc9b058a2ea326b3641d4f65a692667f16e
Jacob <kuba300698@gmail.com>
2018-09-06 00:22:22 +0200

    File save added

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var SerialPort = require("serialport");
+var inByte;
+function onRequest(request, response) {
+  if (request.method == 'POST'){
+    myPort.write('1');
+    setTimeout(()=>{myPort.write('0')}, 3000)
+  }
+  console.log(`A user made a ${request.method} request`);
+  response.writeHead(200, {"Context-Type": "text/plain"});
+  response.write(inByte);
+  response.end();
+}
+function onData(data) {
+    inByte = data;
+    console.log(inByte);  
+}
+
+var myPort = new SerialPort("COM4", {
+  baudRate: 9600,
+})
+
+myPort.on('data', onData);
+
+http.createServer(onRequest).listen(8888);
+console.log('Server is running...');
--- src/graph.js
+++ src/graph.js
@@ -1,30 +1,99 @@
 const Chart = require('chart.js');
+const axios = require('axios');
 
+const serverUrl = 'http://127.0.0.1:8888';
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-    console.log('updated');
+var tempReads = [];
+var thrustReads = [];
+
+function sendData() {
+    axios.post(serverUrl)
 }
 
+function getData() {
+    axios.get(serverUrl)
+        .then(data => {
+            if (!Number.isInteger(data.data)) {
+                data.data = data.data.split()
+                thrustReads.push(data.data[0]);
+                tempReads.push(data.data[1]);
+                updateChart(myChart);
+            }
+        })
+        .catch(err => { console.log(err) });
+}
+
+function updateChart(chart) {
+    chart.data.labels.push(chart.data.datasets[0].data.length + 1);
+    chart.data.datasets[0].data = tempReads;
+    chart.data.datasets[1].data = thrustReads;
+    chart.update();
+}
+
+function toCsv() {
+    var csv = '';
+    for (var i = 0; i < tempReads.length; i++){
+        csv += tempReads[i] + ',' + thrustReads[i];
+    }
+    return csv;
+}
+
+function downloadCSV() {  
+    var data, filename, link;
+    var csv = toCsv();
+
+    filename = 'readouts.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
+}
+
+var interval;
+
+function launch() {
+    launched = true;
+    interval = setInterval(getData, 50);
+    sendData();
+}
+
+function stop() {
+    launched = false;
+    clearInterval(interval);
+    downloadCSV();
+}
+
+
+var btn = document.getElementById("btn");
 var ctx = document.getElementById("chart").getContext('2d');
 
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['1', '2'],
+        labels: [],
         datasets: [{
             label: 'Temperature',
             borderColor: 'rgb(255, 99, 132)',
-            data: [1, 0.5]
+            data: []
+        }, {
+            label: 'Thrust',
+            borderColor: 'rgb(237, 216, 146)',
+            data: []
         }]
     },
     options: {
-        responsive: false
+        responsive: true,
+        legend: {
+            labels: {
+                fontColor: 'white',
+                fontFamily: 'Helvetica'
+            }
+        }
     }
 });
-
-    
